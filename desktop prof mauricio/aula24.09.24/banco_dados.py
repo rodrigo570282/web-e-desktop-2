8b87c6cf62b46c1f9b0006371d017157b2c30c0b
rodrigo570282 <rodrigorrs.sp@gmail.com>
2024-09-24 21:44:18 -0400

    aula 24 de set

---
+++ desktop prof mauricio/aula24.09.24/banco_dados.py
@@ -0,0 +1,14 @@
+
+# create database meu_banco
+# use meu_banco;
+# create table Usuario(
+# id int auto_increment primary key,
+# nome varchar(100),
+# email varchar(100),
+# senha varchar(255)
+# );
+# insert into Usuario(nome, email, senha)
+# values('rodrigo','rodrigorrs.sp@gmail.com', SHA2('123456', 256)),
+# ('MARIA','maria@bonita.com',SHA2('123456', 256));
+
+# select * from Usuario;
